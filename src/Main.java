472f7b5f258fac4d9b84d20351378493d7b6f225
Elmar <elmarblabla@hotmail.com>
2013-02-25 07:42:08 +0100

    Works now

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,39 @@
-public class Main {
+import org.newdawn.slick.*;
+
+public class Main extends BasicGame {
+
+    public Main(String title) {
+        super(title);
+    }
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        //Niews
-        //Update 2
+        try {
+            Main main = new Main("RPG");
+            AppGameContainer app = new AppGameContainer(main);
+            app.setDisplayMode(640, 400, false);
+            app.setAlwaysRender(true);
+            app.setShowFPS(true);
+            app.setVSync(false);
+            app.setVerbose(false);
+            app.setMaximumLogicUpdateInterval(1);
+            app.start();
+        } catch (SlickException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void init(GameContainer gameContainer) throws SlickException {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void update(GameContainer gameContainer, int i) throws SlickException {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void render(GameContainer gameContainer, Graphics graphics) throws SlickException {
+        //To change body of implemented methods use File | Settings | File Templates.
     }
 }
